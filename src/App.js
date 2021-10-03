f840b68dbb0fe87ee16edeafccab03c14efc7f26
Enoch Gyasi <lindakumah03@gmail.com>
2021-10-03 13:08:48 +0000

    react public holidays project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from 'react';
 import LoginContainer from "./components/AuthComponent";
 import HomeContainer from "./components/HomeComponent";
@@ -7,20 +7,15 @@ import SignupContainer from "./components/Signup";
 
 import './App.css';
 import './main.css';
-
 function App() {
   return (
     <div>
-
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={LoginContainer} exact />
-          <Route path="/forget-password" component={ForgetPasswordContainer} exact />
-          <Route path="/sign-up" component={SignupContainer} exact />
-          <Route path="/home" component={HomeContainer} exact />
-        </Switch>
-      </BrowserRouter>
-
+      <Router>
+        <Route path="/" component={LoginContainer} exact />
+        <Route path="/forget-password" component={ForgetPasswordContainer} exact />
+        <Route path="/sign-up" component={SignupContainer} exact />
+        <Route path="/home" component={HomeContainer} exact />
+      </Router>
     </div>
   );
 }
