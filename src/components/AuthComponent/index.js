6490bce2b8d1badf0885ec8244804c803d74c559
Enoch Gyasi <lindakumah03@gmail.com>
2021-10-05 14:14:29 +0000

    Login name changed

--- src/components/AuthComponent/index.js
+++ src/components/AuthComponent/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Login from './Login'
+import Login from './login'
 import { connect } from 'react-redux'
 import { serverLogin } from './../../actions/AuthAction';
 
